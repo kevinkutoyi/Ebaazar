e155b25dc2ec36ae573123b4e346ce9fc3964cb4
kevinkutoyi <kevinkutoyi95@gmail.com>
2022-08-31 15:09:26 +0100

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let CART = "";
-let PATH = "/data/products.json";
+let PATH = "http://127.0.0.1:5501/data/products.json";
+// let PATH = "http://127.0.0.1:5500";
 
 // check for current user
 function getUser() {
@@ -24,19 +25,48 @@ function getUser() {
 }
 
 // Fetch data from products.json and store to local storage
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "Content-Type",
+//     "Authorization"
+//   );
+//   next();
+// });
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 function loadJSON(PATH) {
   var xhr = new XMLHttpRequest();
+
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         let data = JSON.parse(xhr.responseText);
+        // data.use{
+        //   cors({
+
+        //   })
+        // }
         localStorage.setItem("products", JSON.stringify(data));
+
         // JSON.parse(localStorage.getItem("data"));
       } else {
         window.alert("Something went wrong, fetch!!");
       }
     }
   };
+
   xhr.open("GET", PATH, true);
   xhr.send();
 }
